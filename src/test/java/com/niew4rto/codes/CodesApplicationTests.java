156c51ff7d40fe84e049d4a3f4d5a70d36fd3fb4
JanRucinski <jasislav@gmail.com>
2024-12-10 22:27:00 +0100

    chore: added missing files

---
+++ src/test/java/com/niew4rto/codes/CodesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niew4rto.codes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodesApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
